b5ffc4b5ad94f00d91d561d5d73ff0d908d2774b
T J Newman <tjnewman2013@gmail.com>
2016-01-07 22:56:40 +0000

    Very basic start on client header file

---
+++ src/Client.hpp
@@ -0,0 +1,15 @@
+#ifdef CLIENT_H	
+#define CLIENT_H
+
+/*
+To begin with client contains a socket which just connects to server.
+TODO Understand how to setup up boost socket so that connection to ruby server can be made.
+*/
+
+#include <iostream>
+
+class Client {
+
+}
+
+#endif
