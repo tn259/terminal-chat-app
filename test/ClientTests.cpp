8f7ad299ead6b6a4da0f9d968daa5f3f20085333
Tim <tjnewman2013@gmail.com>
2016-01-26 23:18:47 +0000

    Prepare initial gtests

---
+++ test/ClientTests.cpp
@@ -0,0 +1,8 @@
+#include <gtest/gtest.h>
+/*TODO Inject default constructed boost dependency "socket" 
+Check that each method does not throw system_error
+Check that the buffer does not overflow
+Implement using SocketHandler
+Start by creating a test fixture for this
+For now we will treat ClientTests.cpp as the main executable test file
+*/
