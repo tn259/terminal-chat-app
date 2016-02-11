b910f4f026007b37afa5ba02969846071bcec8c8
T J Newman <tjnewman2013@gmail.com>
2016-02-11 00:00:39 +0000

    Make Client async

--- include/Client.hpp
+++ include/Client.hpp
@@ -8,25 +8,41 @@
 #include <string> 
 #include <queue>
 #include <memory>
+#include <boost/asio.hpp>
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/array.hpp>
 
 //Use boost smartpointers
 typedef std::shared_ptr<std::string> string_ptr;
 //All clients have access to the same message queue later for multiple clients
 typedef std::unique_ptr< std::queue<string_ptr> > messageQueue_ptr;
 
+using namespace boost::asio::ip;
+//using namespace boost::asio::ip::tcp;
 
 class Client {
 private:
 	messageQueue_ptr messagesOut;
 	messageQueue_ptr messagesIn;
+	tcp::socket socket_;
+	boost::asio::streambuf input_buffer_;
 /*possible future methods for 2. all to be called in run
 	void readUserInput();
 	void send(boost::asio::ip::tcp::socket&);
 	void readNewMessage(boost::asio::ip::tcp::socket&);
-*/		
+*/
+	void stop();
+	void start_connect(tcp::resolver::iterator endpoint_iter);
+	void handle_connect(const boost::system::error_code& ec,
+      tcp::resolver::iterator endpoint_iter);
+	void start_read();
+	void handle_read(const boost::system::error_code& ec);
+	void start_write();
+	void handle_write(const boost::system::error_code& ec);		
 public:
-	Client();
-	void run();
+	Client(boost::asio::io_service& io_service);
+	//void run();	
+	void start(tcp::resolver::iterator endpoint_iter);
 };
 
 #endif
--- src/Client.cpp
+++ src/Client.cpp
@@ -1,13 +1,14 @@
-#include "../include/SocketHandler.hpp"
+//#include "../include/SocketHandler.hpp"
 #include <iostream>
 #include <utility>
+#include <boost/bind.hpp>
 #include "../include/Client.hpp"
 
 //namespace aliasing to enhance readability
 ////Note: must be sure that namespaces do not clash 
 using namespace boost;
 using namespace boost::asio;
-using namespace boost::asio::ip;
+//using namespace boost::asio::ip;
 //constants in anon namespace good for encapsulation
 namespace {
 	const std::string IP = "127.0.0.1";
@@ -15,26 +16,97 @@ namespace {
 }
 
 
-Client::Client(): messagesOut{}, messagesIn{} {}
+Client::Client(boost::asio::io_service& io_service): messagesOut{}, messagesIn{}, socket_{io_service} {}
 
+void Client::start(tcp::resolver::iterator endpoint_iter)
+  {
+    // Start the connect actor.
+         start_connect(endpoint_iter);
+  }
+
+void Client::stop()
+  {
+    socket_.close();
+  }
+
+void Client::start_connect(tcp::resolver::iterator endpoint_iter) {
+	if(endpoint_iter != tcp::resolver::iterator()) {
+		std::cout << "Trying " << endpoint_iter->endpoint() << "..\n";
+		// Start the asynchronous connect operation.
+		 socket_.async_connect(endpoint_iter->endpoint(),
+		 boost::bind(&Client::handle_connect, this, _1, endpoint_iter));	}
+	else {
+		//No more enpoints
+		stop();
+	}
+}
+
+void Client::handle_connect(const boost::system::error_code& ec,
+	tcp::resolver::iterator endpoint_iter) {
+	if (ec) {
+      		std::cout << "Connect error: " << ec.message() << "\n";
+
+      // We need to close the socket used in the previous connection attempt
+      //       // before starting a new one.
+       		socket_.close();
+      // Try the next available endpoint.
+       		start_connect(++endpoint_iter);
+        } else {
+		std::cout << "Connected to " << endpoint_iter->endpoint() << "\n";
+      	        start_read();
+               // start_write();
+        }
+}
+
+void Client::start_read() {
+    //         // Start an asynchronous operation to read a newline-delimited message.
+                boost::asio::async_read_until(socket_, input_buffer_, '\n',
+	                boost::bind(&Client::handle_read, this, _1));
+}
+
+void Client::handle_read(const boost::system::error_code& ec) {
+	if (!ec) {
+      // Extract the newline-delimited message from the buffer.
+         	std::string line;
+                std::istream is(&input_buffer_);
+                std::getline(is, line);
+      // Empty messages are heartbeats and so ignored.
+     	        if (!line.empty())   {
+                    	 std::cout << "Received: " << line << "\n"; 
+                }
+                start_read();
+        }  else {
+         	 std::cout << "Error on receive: " << ec.message() << "\n";
+      		 stop();
+        }        
+}
+
+void Client::start_write() {
+     //Start an asynchronous operation to send a heartbeat message.
+           boost::asio::async_write(socket_, boost::asio::buffer("\n", 1),
+           boost::bind(&Client::handle_write, this, _1));
+}
+
+void Client::handle_write(const boost::system::error_code& ec) {
+	if(!ec) {
+		std::string line; 
+		std::getline(std::cin, line);
+		start_write();
+	} else {
+		std::cout << ec.message() << "\n";
+		stop();
+	}
+}
+
+/*
 void Client::run() {
 	try{	//query for endpoints matching a server on specific port and ip
-		io_service io_s;
-		tcp::resolver resolver{io_s};
-		tcp::resolver::query query{IP, PORT};
-		tcp::resolver::iterator endpoint = resolver.resolve(query);
-		
-		tcp::socket socket{io_s};
-		
-		SocketHandler sh{socket, endpoint};
-		
-		sh.getMessages();	
 		 
 	} catch (boost::system::system_error& e) {
 		std::cerr << e.what() << std::endl;
 	}	
 }
-
+*/
 /* FOR USE AFTER INITIAL TESTS WRITTEN
 
 May be better to just have listen() and send() which run Two infinite looping threads
@@ -83,7 +155,20 @@ void Client::readNewMessage(tcp::socket& socket) {
 
 
 int main() {
-	Client c{};
-	c.run();
-	return 0;
+	try {
+    		boost::asio::io_service io_service;
+    		tcp::resolver r(io_service);
+    		Client c(io_service);
+
+    		c.start(r.resolve(tcp::resolver::query(IP, PORT)));
+
+    		io_service.run();
+  	}
+  	catch (std::exception& e)
+  	{
+    		std::cerr << "Exception: " << e.what() << "\n";
+  	}
+
+  	return 0;
 }
+
